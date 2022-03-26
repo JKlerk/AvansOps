01e852cf2e87a8c4b1865c502f7e2fd6e0cdf47d
Joey De Klerk <JKlerk>
2022-03-26 20:13:27 +0100

    Added gitignore

--- avansops/Tests/UseCaseTests.cs
+++ avansops/Tests/UseCaseTests.cs
@@ -24,5 +24,16 @@ namespace AvansOps.Tests
         
             Assert.NotEmpty(notificationStrategy.Messages);
         }
+
+        [Fact]
+        public void Test_US_2()
+        {
+            var notificationStrategy = new NotificationSlackProxy();
+            var member = new ProjectMember(new User("Firstname", "Lastname", "test@test.com"), new List<Role>() {Role.Developer, Role.ScrumMaster}, notificationStrategy);
+            var backlogItem = new BackLogItem(1, "Backlogitem 1", "Doe deze stuff");
+        
+            var project = ProjectFactory.CreateProject(1, "Project 1", "description of project", member);
+            Assert.True(project.Name == "Project 1");
+        }
     }
 }
